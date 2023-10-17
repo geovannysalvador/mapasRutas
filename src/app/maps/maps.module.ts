328dcb83c3a597b07ee53aab029b54fdfd6b1076
GeovannyTS <yowtecum@gmail.com>
2023-10-17 10:59:30 -0600

    component desing search & details

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -9,7 +9,9 @@ import { environment } from '../../environments/environment';
 // para ver el mapa
 import Mapboxgl from 'mapbox-gl';
 import { BtnMyLocationComponent } from './components/btn-my-location/btn-my-location.component';
-import { AngularLogoComponent } from './components/angular-logo/angular-logo.component'; // or "const mapboxgl = require('mapbox-gl');"
+import { AngularLogoComponent } from './components/angular-logo/angular-logo.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { SearchResultComponent } from './components/search-result/search-result.component'; // or "const mapboxgl = require('mapbox-gl');"
 
 (Mapboxgl as any).accessToken =  environment.mapbox_key
 
@@ -21,6 +23,8 @@ import { AngularLogoComponent } from './components/angular-logo/angular-logo.com
     LoadingComponent,
     BtnMyLocationComponent,
     AngularLogoComponent,
+    SearchBarComponent,
+    SearchResultComponent,
   ],
   imports: [
     CommonModule,
