a2c4bb6a225b88aa12dd7e647463e8f0f37b1be9
GeovannyTS <yowtecum@gmail.com>
2023-10-17 11:58:30 -0600

    Implements Debounce input

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class SearchBarComponent {
 
+  private debounceTimer?:NodeJS.Timeout;
+
+  onQueryChanged( query:string = '' ){
+
+    if( this.debounceTimer ) clearTimeout( this.debounceTimer );
+
+    this.debounceTimer = setTimeout(() => {
+
+      console.log('Mandar este queey', query);
+
+
+    }, 400);
+
+
+
+  }
+
 }
